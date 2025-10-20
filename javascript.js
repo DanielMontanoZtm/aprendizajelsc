96fe8664059498f5abf3adf15231d2eb90b1948e
DanielMontanoZtm <Danztm0.0@gmail.com>
2025-10-19 23:36:51 -0500

    Upload 8

--- javascript.js
+++ javascript.js
@@ -2,6 +2,8 @@ document.addEventListener('DOMContentLoaded', function() {
 
 });
 
+
+
 document.addEventListener('DOMContentLoaded', function() {
     // ===== BLOQUE 1: Lógica del menú activo =====
     (function initMenu() {
